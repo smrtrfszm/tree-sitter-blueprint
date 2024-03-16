cc1e151fe466014c998b6de25eb33c90e352ed97
Szepesi Tibor <sztibi023@gmail.com>
2024-03-16 18:37:49 +0100

    Add basic highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,96 @@
+(comment) @comment
+
+(using
+  namespace: (ident) @type)
+
+(type_name
+  name: (ident) @type)
+(type_name
+  namespace: (ident) @type)
+
+(object
+  id: (ident) @variable)
+(closure_expression
+  name: (ident) @variable)
+(lookup_expression (ident) @property)
+
+(property
+  name: (ident) @property)
+(ext_adw_dialog_response
+  id: (ident) @property)
+(ext_adw_breakpoint
+  property: (ident) @property)
+(ext_combo_box_items
+  id: (ident) @property)
+(ext_accessibility
+  name: (ident) @property)
+(ext_layout
+  name: (ident) @property)
+(menu_attribute
+  name: (ident) @property)
+
+(ext_adw_breakpoint
+  object: (ident) @property)
+
+(signal
+  handler: (ident) @function)
+
+(escape_sequence) @string.escape
+
+(number) @number
+(quoted) @string
+
+((literal (ident) @constant.builtin)
+  (#any-of? @constant.builtin "true" "false" "null"))
+
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+  "<"
+  ">"
+] @punctuation.bracket
+
+[
+ ":"
+ ";"
+ "."
+ ","
+ "::"
+ "|"
+ "=>"
+] @punctuation.delimiter
+
+"$" @punctuation.special
+
+[
+  "using"
+  "template"
+  "menu"
+  "submenu"
+  "section"
+  "item"
+  "accessibility"
+  "styles"
+  "bind"
+  "bind-property"
+  "responses"
+  "condition"
+  "setters"
+  "items"
+  "as"
+  "mime-types"
+  "patterns"
+  "suffixes"
+  "layout"
+  "marks"
+  "mark"
+  "widgets"
+  "strings"
+  "translation-domain"
+  "typeof"
+] @keyword
+
