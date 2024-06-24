63478fec21bcfe1bab3ea873940aaaee90d1020e
Szepesi Tibor <smrtrfszm@proton.me>
2024-06-24 11:58:07 +0200

    Add indent queries

---
+++ queries/indents.scm
@@ -0,0 +1,63 @@
+[
+  (object_content)
+  (menu)
+  (menu_submenu)
+  (menu_section)
+  (menu_item)
+  (menu_item_shorthand)
+  (ext_accessibility)
+  (ext_adw_dialog)
+  (ext_adw_breakpoint)
+  (ext_adw_breakpoint_condition)
+  (ext_combo_box_items)
+  (ext_file_filter_mime_types)
+  (ext_file_filter_patterns)
+  (ext_file_filter_suffixes)
+  (ext_layout)
+  (ext_scale_marks)
+  (ext_scale_mark)
+  (ext_size_group_widgets)
+  (ext_string_list_strings)
+  (ext_styles)
+] @indent.begin
+
+(object_content
+  "}" @indent.branch @indent.dedent)
+(menu
+  "}" @indent.branch @indent.dedent)
+(menu_submenu
+  "}" @indent.branch @indent.dedent)
+(menu_section
+  "}" @indent.branch @indent.dedent)
+(menu_item
+  "}" @indent.branch @indent.dedent)
+(menu_item_shorthand
+  ")" @indent.branch @indent.dedent)
+(ext_accessibility
+  "}" @indent.branch @indent.dedent)
+(ext_adw_dialog
+  "]" @indent.branch @indent.dedent)
+(ext_adw_breakpoint
+  "}" @indent.branch @indent.dedent)
+(ext_adw_breakpoint_condition
+  ")" @indent.branch @indent.dedent)
+(ext_combo_box_items
+  "]" @indent.branch @indent.dedent)
+(ext_file_filter_mime_types
+  "]" @indent.branch @indent.dedent)
+(ext_file_filter_patterns
+  "]" @indent.branch @indent.dedent)
+(ext_file_filter_suffixes
+  "]" @indent.branch @indent.dedent)
+(ext_layout
+  "}" @indent.branch @indent.dedent)
+(ext_scale_marks
+  "]" @indent.branch @indent.dedent)
+(ext_scale_mark
+  ")" @indent.branch @indent.dedent)
+(ext_size_group_widgets
+  "]" @indent.branch @indent.dedent)
+(ext_string_list_strings
+  "]" @indent.branch @indent.dedent)
+(ext_styles
+  "]" @indent.branch @indent.dedent)
